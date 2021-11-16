f6674b3c03ead818d5ec3ee97f8a73fc6f1ed3e4
molel <kapitov.04@gmail.com>
2021-11-16 13:14:35 +0300

    Добавил код проекта и отчет

---
+++ ПИ20-4 Капитов ПВ Контекстное меню/context_menu/app/src/main/java/com/example/context_menu/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.context_menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvSize, tvColor;
+
+    final int MENU_COLOR_RED = 1;
+    final int MENU_COLOR_GREEN = 2;
+    final int MENU_COLOR_BLUE = 3;
+
+    final int MENU_SIZE_22 = 4;
+    final int MENU_SIZE_26 = 5;
+    final int MENU_SIZE_30 = 6;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvColor = (TextView) findViewById(R.id.tvColor);
+        tvSize = (TextView) findViewById(R.id.tvSize);
+
+        // для tvColor и tvSize необходимо создавать контекстное меню
+        registerForContextMenu(tvColor);
+        registerForContextMenu(tvSize);
+
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        // TODO Auto-generated method stub
+        switch (v.getId()) {
+            case R.id.tvColor:
+                menu.add(0, MENU_COLOR_RED, 0, "Red");
+                menu.add(0, MENU_COLOR_GREEN, 0, "Green");
+                menu.add(0, MENU_COLOR_BLUE, 0, "Blue");
+                break;
+            case R.id.tvSize:
+                menu.add(0, MENU_SIZE_22, 0, "22");
+                menu.add(0, MENU_SIZE_26, 0, "26");
+                menu.add(0, MENU_SIZE_30, 0, "30");
+                break;
+        }
+    }
+
+}
